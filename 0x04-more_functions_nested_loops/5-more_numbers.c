902b971c968c82a98768a5be78ad1fb3d3cc379c
Opeyemi Adedotun <ope_adedotun@live.com>
2023-07-20 20:45:31 -0700

    UPdate

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,9 @@
 #include "main.h"
+#include "stdio.h"
+
 /**
  * more_numbers - a function that prints 10 times the numbers, from 0 to 14
- *
+ *followed by a new line
 */
 
 void more_numbers(void)
