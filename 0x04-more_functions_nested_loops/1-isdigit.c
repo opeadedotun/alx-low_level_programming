a53b32fa8197fd9bcede21c3179d7797c7e53420
Opeyemi Adedotun <ope_adedotun@live.com>
2023-07-20 19:06:18 -0700

    a function that checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - funtion to check for a digit (0 through 9)
+ * @c: Variable text
+ * Return: (1) if true and (0) if false
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
